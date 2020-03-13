258068ee4a027ba25c379b6cf927472bf59d130b
Sean Bray <ucapsb5@ucl.ac.uk>
2020-03-13 20:37:25 +0000

    Adding testing for NormalEquations class

--- Code/Lib/lrgLinearModelSolverStrategyI.h
+++ Code/Lib/lrgLinearModelSolverStrategyI.h
@@ -1,8 +1,10 @@
 /** lrgLinearModelSolverStrategyI.h**/
+#ifndef lrgLinearModelSolverStrategyI_h
+#define lrgLinearModelSolverStrategyI_h
 #include <vector>
 class LinearModelSolverStrategy {
         public:
             virtual std ::pair<double, double>  FitData(std::vector<std::pair<double, double> >)=0;
 };
 
-
+#endif
--- Testing/lrgLeastSquaresSolverTests.cpp
+++ Testing/lrgLeastSquaresSolverTests.cpp
@@ -16,11 +16,14 @@
 #include "lrgCatchMain.h"
 #include "lrgLinearDataCreator.h"
 #include "lrgNormalEquationsSolverStrategy.h"
+#include "lrgGradientDescentSolverStrategy.h"
 #include <iostream>
 #include <vector>
+#include <math.h>
 
  LinearDataCreator data(1,2,1000);
- NormalEquationsSolverStrategy output;
+ NormalEquationsSolverStrategy NE;
+ GradientDescentSolverStrategy GD(std::make_pair(1, 1),0.1,1000);
  std::vector<double> xvec;
 
 TEST_CASE( "Checking the number of returned items is correct", "[LinearDataCreator.GetData()]" ) {
@@ -32,4 +35,14 @@ TEST_CASE( "Checking the distribution of returned items is correct", "[LinearDat
   xvec.push_back( data.GetData()[i].first);
   REQUIRE( *std::max_element(xvec.begin(),xvec.end()) <= 10); //checking GetData() simulates explanatory variable in the range 0-1000
   REQUIRE( *std::min_element(xvec.begin(),xvec.end()) >= 0);
+}
+
+TEST_CASE( "Checking NE FitData returns finite results", "[NormalEquationsSolverStrategy]" ) { 
+  REQUIRE( isinf(NE.FitData(data.GetData()).first) == 0); 
+  REQUIRE( isinf(NE.FitData(data.GetData()).second) == 0);
+}
+
+TEST_CASE( "Checking NE FitData returns not nan results", "[NormalEquationsSolverStrategy]" ) { 
+  REQUIRE( isnan(NE.FitData(data.GetData()).first) == 0); 
+  REQUIRE( isnan(NE.FitData(data.GetData()).second) == 0);
 }
