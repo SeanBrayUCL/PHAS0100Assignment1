51280cae4ac3760209cb9cb8a3c70ec607e7be41
Sean Bray <ucapsb5@ucl.ac.uk>
2020-03-13 16:27:10 +0000

    Updating LinearDataGeneration GetData() method, to simulate floating x values from 0 to 10, with noise from 0 to 1, and updating tests

--- Testing/lrgLeastSquaresSolverTests.cpp
+++ Testing/lrgLeastSquaresSolverTests.cpp
@@ -19,19 +19,17 @@
 #include <iostream>
 #include <vector>
 
- LinearDataCreator data(2,100,10);
+ LinearDataCreator data(1,2,1000);
  NormalEquationsSolverStrategy output;
  std::vector<double> xvec;
 
-
- 
 TEST_CASE( "Checking the number of returned items is correct", "[LinearDataCreator.GetData()]" ) {
-  REQUIRE( data.GetData().size() == 10); 
+  REQUIRE( data.GetData().size() == 1000); 
 }
 
 TEST_CASE( "Checking the distribution of returned items is correct", "[LinearDataCreator.GetData()]" ) { 
   for (int i=0; i<data.getnumpoints(); i++) 
   xvec.push_back( data.GetData()[i].first);
-  REQUIRE( *std::max_element(xvec.begin(),xvec.end()) <= 1000); //checking GetData() simulates explanatory variable in the range 0-1000
+  REQUIRE( *std::max_element(xvec.begin(),xvec.end()) <= 10); //checking GetData() simulates explanatory variable in the range 0-1000
   REQUIRE( *std::min_element(xvec.begin(),xvec.end()) >= 0);
 }
