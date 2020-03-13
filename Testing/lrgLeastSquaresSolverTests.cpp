b5f2bc1ec4d1dadba82e1831c2d4eb5fc6be9dcb
Sean Bray <ucapsb5@ucl.ac.uk>
2020-03-13 07:01:50 +0000

    Creating seperate test cases in the test file

--- Testing/lrgLeastSquaresSolverTests.cpp
+++ Testing/lrgLeastSquaresSolverTests.cpp
@@ -18,12 +18,16 @@
 #include <iostream>
 #include <vector>
 
-TEST_CASE( "My first test", "[some group identifier]" ) {
-  LinearDataCreator data(2,100,10);
-  std::vector<double> xvec;
+ LinearDataCreator data(2,100,10);
+ std::vector<double> xvec;
+ 
+TEST_CASE( "Checking the number of returned items is correct", "[LinearDataCreator.GetData()]" ) {
+  REQUIRE( data.GetData().size() == 10); 
+}
+
+TEST_CASE( "Checking the distribution of returned items is correct", "[LinearDataCreator.GetData()]" ) { 
   for (int i=0; i<data.getnumpoints(); i++) 
-    xvec.push_back( data.GetData()[i].first);
-  REQUIRE( data.GetData().size() == 10); //checking GetData creates data of the specified size 
+  xvec.push_back( data.GetData()[i].first);
   REQUIRE( *std::max_element(xvec.begin(),xvec.end()) <= 1000); //checking GetData() simulates explanatory variable in the range 0-1000
   REQUIRE( *std::min_element(xvec.begin(),xvec.end()) >= 0);
 }
